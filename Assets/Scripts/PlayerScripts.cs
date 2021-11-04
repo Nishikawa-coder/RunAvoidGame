c00aea7be0de51c86a70f9c2d1a99a497bf8a943
Nishikwa-coder <ausaigo115@au.com>
2021-11-04 23:46:42 +0900

    create new file

---
+++ Assets/Scripts/PlayerScripts.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScripts : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    public float speed = 2;
+    float vx = 0;
+    bool leftFlag = false;
+
+    //デフォルトで右向きキャラが左向きになるように設定されている。
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vx = 0;
+        if (Input.GetKey("right"))
+        {
+            vx = speed;
+            leftFlag = false;
+        }
+        if (Input.GetKey("left"))
+        {
+            vx = -speed;
+            leftFlag = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        this.transform.Translate(vx / 50, 0, 0);
+        this.GetComponent<SpriteRenderer>().flipX = leftFlag;
+    }
+}
