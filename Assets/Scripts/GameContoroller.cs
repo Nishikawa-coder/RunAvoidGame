060eb3cfd9d8238ccdc4914a8feb9a8990542c69
Nishikwa-coder <ausaigo115@au.com>
2021-11-08 16:22:32 +0900

    set hearts

---
+++ Assets/Scripts/GameContoroller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameContoroller : MonoBehaviour
+{
+    public PlayerScripts chara;
+    public LifePanel lifePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // lifePanel.UpdateLife(chara.Life());
+    }
+}
