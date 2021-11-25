5223aa733b1e84cd6f27fbbc40ebade247f3fee8
Nishikwa-coder <ausaigo115@au.com>
2021-11-25 11:30:11 +0900

    make foeverchase

--- Assets/Scripts/AppearScript.cs
+++ Assets/Scripts/AppearScript.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class AppearScript : MonoBehaviour
 {
-    public float playerposition_x;
-    [SerializeField] private GameObject foreverchaseObject;
+    public float[] playerposition_x;
+    [SerializeField] private GameObject[] foreverchaseObjects;
     [SerializeField] private GameObject player;
+    private int objectNumber=0;
 
     // Start is called before the first frame update
     void Start()
     {
-        foreverchaseObject.SetActive(false);
+        foreach ( var foreverchaseObject in foreverchaseObjects)
+        {
+            foreverchaseObject.SetActive(false);
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.transform.position.x>=playerposition_x)
+        if(player.transform.position.x>=playerposition_x[objectNumber] && objectNumber != foreverchaseObjects.Length - 1)
         {
-            foreverchaseObject.SetActive(true);
+            foreverchaseObjects[objectNumber].SetActive(true);
+            objectNumber +=1;
+        }
+        else if(player.transform.position.x>=playerposition_x[objectNumber])
+        {
+            foreverchaseObjects[objectNumber].SetActive(true);
+        }
+    }
+    public void Disappear()
+    {
+        foreach ( var foreverchaseObject in foreverchaseObjects)
+        {
+            foreverchaseObject.SetActive(false);
+            int i=0;
+            Debug.Log(i);
+            i+=1;
         }
     }
 }
---
+++ Assets/Scripts/Hikari2Script.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hikari2Script : MonoBehaviour
+{
+    [SerializeField] GameObject chasecontroller;
+    [SerializeField] GameObject goalText;
+    public int NextScene = 3;
+    private string hideObjectName = "hikari";
+    public float delaytime = 2.0f;
+    void Start()
+    {
+        goalText.SetActive(false);
+    }
+
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("hikari"))
+        {
+            chasecontroller.GetComponent<AppearScript>().Disappear();
+            GameObject hideObject = GameObject.Find(hideObjectName);
+            hideObject.SetActive(false);
+            goalText.SetActive(true);
+            Invoke(nameof(DelayMethod), delaytime);
+        }
+    }
+    void DelayMethod()
+    {
+        FadeManageScript.FadeOut(NextScene);
+    }
+
+}
