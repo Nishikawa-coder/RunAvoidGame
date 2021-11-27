d37a8f77fc13f740916f3f5c8816493986d95ecf
Nishikwa-coder <ausaigo115@au.com>
2021-11-27 12:01:47 +0900

    make last event scene

--- Assets/Scripts/CameraLock.cs
+++ Assets/Scripts/CameraLock.cs
@@ -6,24 +6,23 @@ public class CameraLock : MonoBehaviour
 {
     public string playername;
     private GameObject player;  //プレイヤー情報格納用
-    private Vector3 offset; // 相対距離取得用
+    public float distance = 40.0f; // 相対距離取得用
     public float max_x;
     // Start is called before the first frame update
     void Start()
     {
         //　Playerの情報を取得
         this.player = GameObject.Find(playername);
-        offset.x = 40.0f;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        if (transform.position.x < max_x && player.transform.position.x>=0.0f)
+        if (player.transform.position.x < max_x && player.transform.position.x>=0.0f)
         {
             Vector3 position = transform.position;
         //　新しいトランスフォームの値を代入する
-            position.x = player.transform.position.x + offset.x;
+            position.x = player.transform.position.x + distance;
             transform.position = position;
         }
 
---
+++ Assets/Scripts/TextDisplayNoMoveScene.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TextDisplayNoMoveScene : MonoBehaviour
+{
+    [SerializeField] GameObject spaceObject;
+    public float delaytime = 1.0f;
+    // public int NextScene = 3;
+    public string[] texts;
+    int textNumber;
+    string displayText;
+    int textCharNumber;
+    
+    void Start()
+    {
+    }
+    void Update()
+    {
+        
+        if (textCharNumber != texts[textNumber].Length) 
+        {
+            //文章を1文字ずつ表示
+            displayText = displayText + texts[textNumber][textCharNumber];
+            textCharNumber = textCharNumber + 1;
+        }
+        else
+        {
+            if (textNumber != texts.Length - 1)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    displayText = "";
+                    textCharNumber = 0;
+                    textNumber = textNumber + 1;
+                }
+            }else
+            {
+                spaceObject.SetActive(false);
+                Invoke(nameof(DelayMethod), delaytime);
+            }
+        }
+        this.GetComponent<Text>().text = displayText;
+        
+    }
+    void DelayMethod()
+    {
+        // FadeManageScript.FadeOut(NextScene);
+    }
+}
